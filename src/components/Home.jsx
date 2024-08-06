5f104c7d259af8ce8834273e60a174a841ff94d1
Hukke1990 <139823467+Hukke1990@users.noreply.github.com>
2024-08-05 22:47:19 -0300

    login ok

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import appFirebase from '../credenciales'
+import { getAuth, signOut } from 'firebase/auth'
 
-export const Home = () => {
+const auth = getAuth(appFirebase)
+
+export const Home = ({ correoUsuario }) => {
+
   return (
-    <div>
-        <h2>Hola Mundo</h2>
+    <div className='container'>
+      <p>Bienvenido, <strong>{correoUsuario}</strong> Has iniciado sesion </p>
+
+      <button className='btnLogout' onClick={() => signOut(auth)}>
+        Cerrar Sesion
+      </button>
+
+      <hr />
+
+      <div className='row'>
+        {/* esta seccion sera del formulario */}
+        <div className='col-md-4'>
+          <h3>Ingresar usuario</h3>
+          <form>
+            <div className="card card-body">
+              <div className="form-group">
+                <input type="text" name='nombre' className='form-control' placeholder='nombre' />
+
+              </div>
+            </div>
+          </form>
+        </div>
+        {/* esta seccion sera la lista de nuestros usuarios */}
+        <div className="col-md-8">
+
+        </div>
+
+      </div>
     </div>
   )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,29 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import imagenFondo from '../assets/fondo.jpg'
-import imagenAvatar from '../assets/avatar.png'
+import imagenAvatar from '../assets/logo-medicina.png'
 
+import appFirebase from '../credenciales'
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+const auth = getAuth(appFirebase)
 
 
 export const Login = () => {
+
+
+  const [registrando, setRegistrando] = useState(false)
+
+  const functAutenticacion = async (e) => {
+    e.preventDefault();
+    const correo = e.target.email.value;
+    const password = e.target.password.value;
+
+    if (registrando) {
+      try {
+        await createUserWithEmailAndPassword(auth, correo, password)
+      } catch (error) {
+        alert('Asegurese de que la contraseña sea de al menos 8 caracteres')
+      }
+    }
+    else {
+      try {
+        await signInWithEmailAndPassword(auth, correo, password)
+      } catch (error) {
+        alert('Correo o contraseña incorrectos')
+      }
+    }
+  }
+
   return (
-    <div className='container'>
+    <div className='contenedor'>
       <div className="row">
-        <div className="col-md-4">
+        <div className="contenedor-login">
           <div className="padre">
-            <div className="card card-body">
+            <div className="card card-body shadow-lg">
               <img src={imagenAvatar} alt="" className='estilo-profile' />
-              <form>
-                <input type="text" placeholder='Ingresar Email' className='cajatexto' />
-                <input type="pass" placeholder='Ingresar Contraseña' className='cajatexto' />
-                <button className='btnfrom'>registrarse</button>
+              <form onSubmit={functAutenticacion}>
+                <input type="text" placeholder='Email' className='cajatexto' id='email' />
+                <input type="password" placeholder='Contraseña' className='cajatexto' id='password' />
+                <button className='btnfrom'>{registrando ? 'Registrate' : 'Iniciar Sesion'}</button>
               </form>
-              <h4><button>Ingresar</button></h4>
+              <h4 className='texto'>{registrando ? 'Ya tienes una cuenta?' : 'No tienes una cuenta?'}<button className='btnswitch' onClick={() => setRegistrando(!registrando)}>{registrando ? 'Iniciar Sesion' : 'Registrate'}</button></h4>
             </div>
           </div>
-        </div>
-
-        <div className="col-md-8">
-          <img src={imagenFondo} alt="" className='tamanio-imagen' />
         </div>
       </div>
     </div>
