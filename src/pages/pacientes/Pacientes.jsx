f229649eead381bf60176a44366dc67c47e653cd
Hukke1990 <139823467+Hukke1990@users.noreply.github.com>
2024-08-08 01:19:05 -0300

    footer ok

--- src/pages/pacientes/Pacientes.jsx
+++ src/pages/pacientes/Pacientes.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import './Pacientes.css'
+import { NavLink } from 'react-router-dom'
 
 export const Pacientes = () => {
     return (
-        <div>
-            <h1>Pacientes</h1>
-            <h2>En construccion</h2>
-        </div>
+        <div className='contenedor-pacientes'>
+            <div className='padre-pacientes'>
+                <div className='padre-titulo'>
+                    <h1>Pacientes</h1>
+                </div>
+
+                <ul>
+                    <li><NavLink to='/'>Registro</NavLink></li>
+                    <li><NavLink to='/'>Diagnostico</NavLink></li>
+                    <li><NavLink to='/'>Historial Clinico</NavLink></li>
+                    <li><NavLink to='/'>Citas</NavLink></li>
+                </ul>
+
+            </div>
+        </div >
     )
 }
