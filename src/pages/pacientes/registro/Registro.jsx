f1d5a2379d92e799359c0cdc65ac58727e00b3bf
Hukke1990 <139823467+Hukke1990@users.noreply.github.com>
2024-08-08 17:52:28 -0300

    formulario registro ok!

--- src/pages/pacientes/registro/Registro.jsx
+++ src/pages/pacientes/registro/Registro.jsx
@@ -1,33 +1,200 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { getFirestore, collection, addDoc } from 'firebase/firestore'
+import appFirebase from '../../../../src/credenciales'
 import './Registro.css'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
+
+const db = getFirestore(appFirebase);
 
 export const Registro = () => {
+    const [formValues, setFormValues] = useState({
+        nombre: '',
+        apellido: '',
+        fechaNacimiento: '',
+        sexo: '',
+        obraSocial: '',
+        plan: '',
+        carnet: '',
+        dni: '',
+        provincia: '',
+        ciudad: '',
+        direccion: '',
+        telefono: '',
+        email: '',
+    });
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setFormValues({
+            ...formValues,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // Filtrar campos vacíos
+        const filteredValues = Object.fromEntries(
+            Object.entries(formValues).filter(([key, value]) => value.trim() !== '')
+        );
+
+        if (Object.keys(filteredValues).length === 0) {
+            alert('Todos los campos están vacíos.');
+            return;
+        }
+
+        try {
+            await addDoc(collection(db, 'pacientes'), filteredValues);
+            alert('Paciente registrado con éxito');
+            navigate('/pacientes');
+        } catch (error) {
+            console.error('Error al registrar paciente:', error);
+            alert('Hubo un error al registrar el paciente');
+        }
+    };
+
     return (
         <div className='contenedor-registro'>
             <div className='padre-registro'>
                 <h1>Registrar paciente</h1>
-                <form className='form-registro'>
+                <form className='form-registro' onSubmit={handleSubmit}>
                     <fieldset className='fieldset-registro'>
                         <div className='inputs-registro'>
-                            <input type="text" placeholder='Nombre' />
-                            <input type="text" placeholder='Apellido' />
-                            <input type="text" placeholder='Fecha de nacimiento' />
-                            <input type="text" placeholder='Sexo' />
-                            <input type="text" placeholder='Obra Social' />
-                            <input type="text" placeholder='Plan' />
-                            <input type="text" placeholder='Carnet' />
-                            <input type="text" placeholder='D.N.I' />
-                            <input type="text" placeholder='Provincia' />
-                            <input type="text" placeholder='Ciudad' />
-                            <input type="text" placeholder='Direccion' />
-                            <input type="text" placeholder='Telefono' />
-                            <input type="email" placeholder='Email' />
+                            <div>
+                                <label htmlFor="nombre">Nombre</label>
+                                <input
+                                    type="text"
+                                    name="nombre"
+                                    placeholder='Nombre'
+                                    value={formValues.nombre}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="apellido">Apellido</label>
+                                <input
+                                    type="text"
+                                    name="apellido"
+                                    placeholder='Apellido'
+                                    value={formValues.apellido}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="fechaNacimiento">Fecha de Nacimiento</label>
+                                <input
+                                    type="date"
+                                    name="fechaNacimiento"
+                                    placeholder='Fecha de Nacimiento'
+                                    value={formValues.fechaNacimiento}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="sexo">Sexo</label>
+                                <select name="sexo" id="">
+                                    <option value="">Seleccione una opcion</option>
+                                    <option value="Masculino">Masculino</option>
+                                    <option value="Femenino">Femenino</option>
+                                </select>
+                            </div>
+                            <div>
+                                <label htmlFor="obraSocial">Obra Social</label>
+                                <input
+                                    type="text"
+                                    name="obraSocial"
+                                    placeholder='Obra Social'
+                                    value={formValues.obraSocial}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="plan">Plan</label>
+                                <input
+                                    type="text"
+                                    name="plan"
+                                    placeholder='Plan'
+                                    value={formValues.plan}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="carnet">Carnet</label>
+                                <input
+                                    type="text"
+                                    name="carnet"
+                                    placeholder='Carnet'
+                                    value={formValues.carnet}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="dni">DNI</label>
+                                <input
+                                    type="text"
+                                    name="dni"
+                                    placeholder='D.N.I'
+                                    value={formValues.dni}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="provincia">Provincia</label>
+                                <input
+                                    type="text"
+                                    name="provincia"
+                                    placeholder='Provincia'
+                                    value={formValues.provincia}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="ciudad">Ciudad</label>
+                                <input
+                                    type="text"
+                                    name="ciudad"
+                                    placeholder='Ciudad'
+                                    value={formValues.ciudad}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="direccion">Dirección</label>
+                                <input
+                                    type="text"
+                                    name="direccion"
+                                    placeholder='Direccion'
+                                    value={formValues.direccion}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="telefono">Telefono</label>
+                                <input
+                                    type="text"
+                                    name="telefono"
+                                    placeholder='Telefono'
+                                    value={formValues.telefono}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="email">Email</label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    placeholder='Email'
+                                    value={formValues.email}
+                                    onChange={handleChange}
+                                />
+                            </div>
                         </div>
                     </fieldset>
                     <div className='botones-registro'>
-                        <button className='boton-registro'>Registrar</button>
-                        <button className='boton-registro cancelar'><NavLink to='/pacientes'>Cancelar</NavLink></button>
+                        <button type="submit" className='boton-registro'>Registrar</button>
+                        <button type="button" className='boton-registro cancelar'><NavLink to='/pacientes'>Cancelar</NavLink></button>
                     </div>
                 </form>
             </div>
