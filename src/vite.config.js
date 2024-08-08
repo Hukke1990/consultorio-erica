af267e484d5440ccb4e7747d11bb87fc4edcdcb1
Hukke1990 <139823467+Hukke1990@users.noreply.github.com>
2024-08-07 22:28:20 -0300

    vite.config y meta.env

---
+++ src/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // El directorio de salida para la compilación
+  },
+});
