1b0038097dc483b63f3185ed41622925d3885396
Hukke1990 <139823467+Hukke1990@users.noreply.github.com>
2024-08-10 19:55:59 -0300

    header listo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Header.css'
 
 export const Header = (correoUsuario) => {
     return (
-        <div>
+        <div className='contenedor-header'>
             <h1>Bienvenido <span>{correoUsuario.correoUsuario}</span></h1>
         </div>
     )
