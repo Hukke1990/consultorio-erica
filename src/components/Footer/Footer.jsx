a773960301cce72c8b46c402850109a3063f9eb8
Hukke1990 <139823467+Hukke1990@users.noreply.github.com>
2024-08-10 23:55:38 -0300

    revisar footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import './Footer.css'
 
 export const Footer = () => {
     return (
-        <footer>
-            <p>© 2023. Todos los derechos reservados.</p>
+        <footer className='footer'>
+            <p>© 2024. Todos los derechos reservados.</p>
         </footer>
     )
 }
