58902128857d6988272c32e97959cbae82cf09ff
Hukke1990 <gastonbordet@gmail.com>
2024-09-10 18:40:00 -0300

    asd

--- src/pages/pacientes/Pacientes.jsx
+++ src/pages/pacientes/Pacientes.jsx
@@ -1,25 +1,34 @@
-import React from 'react'
-import './Pacientes.css'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import "./Pacientes.css";
+import { NavLink } from "react-router-dom";
 
 export const Pacientes = () => {
-    return (
-        <div className='contenedor-pacientes'>
-            <div className='padre-pacientes'>
-                <div className='padre-titulo titulo'>
-                    <h1>Pacientes</h1>
-                </div>
+  return (
+    <div className="contenedor-pacientes">
+      <div className="padre-pacientes">
+        <div className="padre-titulo titulo">
+          <h1>Pacientes</h1>
+        </div>
 
-                <div className='contenedor-opcionesPacientes'>
-                    <ul>
-                        <li><NavLink to='/pacientes/registro'>Registro</NavLink></li>
-                        <li><NavLink to='/pacientes/verPacientes'>Ver pacientes</NavLink></li>
-                        <li><NavLink to='/pacientes/diagnostico'>Diagnostico</NavLink></li>
-                        <li><NavLink to='/pacientes/historialClinico'>Historial Clinico</NavLink></li>
-                    </ul>
-                </div>
-
-            </div>
-        </div >
-    )
-}
+        <div className="contenedor-opcionesPacientes">
+          <ul>
+            <li>
+              <NavLink to="/pacientes/registro">Registro</NavLink>
+            </li>
+            <li>
+              <NavLink to="/pacientes/verPacientes">Ver pacientes</NavLink>
+            </li>
+            <li>
+              <NavLink to="/pacientes/diagnostico">Diagnostico</NavLink>
+            </li>
+            <li>
+              <NavLink to="/pacientes/historialClinico">
+                Historial Clinico
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
